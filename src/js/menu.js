64e941a53797c52f8f494094fed6a24a4ba6629e
Alexmiko <mr.coctail@mail.ru>
2022-05-03 22:01:26 +0300

    second finally rights

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,19 +1,17 @@
 
 document.querySelector(".menu-burger").addEventListener("touchstart", () => {
-  document.querySelector(".menu-burger").classList.toggle("active");
-  document.querySelector(".lettering__list").classList.toggle("active");
+	document.querySelector(".menu-burger").classList.toggle("active");
+	document.querySelector(".lettering__list").classList.toggle("active");
+	document.body.classList.toggle("disable-scroll");
 });
+
+document.querySelectorAll(`.lettering__item`).forEach((menuItem) => {
+	menuItem.addEventListener(`click`, () => {
+		document.body.classList.remove("disable-scroll");
+	})
+})
 
 window.addEventListener('resize', () => {
-  let vh = window.visualViewport.height * 0.01;
-  document.querySelector(".lettering__list").style.setProperty('--vh', `${vh}px`);
+	let vh = window.visualViewport.height * 0.01;
+	document.querySelector(".lettering__list").style.setProperty('--vh', `${vh}px`);
 });
-
-function removeMenu() {
-  let scroll = window.pageYOffset;
-  if (scroll > 300) {
-    document.querySelector(".lettering__list").classList.remove("active");
-    document.querySelector(".menu-burger").classList.remove("active");
-  }
-}
-window.addEventListener('scroll', removeMenu, true);
--- src/js/swipers.js
+++ src/js/swipers.js
@@ -1,163 +1,163 @@
 const portfolio01 = document.querySelector(".portfolio-swiper-one");
-if(portfolio01){
-const portfolioSwiper01 = new Swiper('.portfolio-swiper-one', {
-  spaceBetween: 25,
-  wrapperClass: "portfolio-swiper-wrapper-one",
-  slideClass: "portfolio-swiper-slide-one",
-  loop: true,
-  grabCursor: true,
-  observer: true,
-  observeParents: true,
-  slidesPerView:1.2,
+if (portfolio01) {
+	const portfolioSwiper01 = new Swiper('.portfolio-swiper-one', {
+		spaceBetween: 25,
+		wrapperClass: "portfolio-swiper-wrapper-one",
+		slideClass: "portfolio-swiper-slide-one",
+		loop: true,
+		grabCursor: true,
+		observer: true,
+		observeParents: true,
+		slidesPerView: 1.2,
 
-  navigation: {
-    nextEl: ".portfolio-button__next",
-    prevEl: ".portfolio-button__prev",
-  },
-  breakpoints: {
-    500: {
-      slidesPerView: 1.5,
-    },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1440: {
-      slidesPerView: 2.2,
-    },
-    1920: {
-      slidesPerView: 2.7,
-    },
-  }
-});
+		navigation: {
+			nextEl: ".portfolio-button__next",
+			prevEl: ".portfolio-button__prev",
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 1.5,
+			},
+			1024: {
+				slidesPerView: 2,
+				spaceBetween: 30,
+			},
+			1440: {
+				slidesPerView: 2.2,
+			},
+			1920: {
+				slidesPerView: 2.7,
+			},
+		}
+	});
 }
 
 const portfolio02 = document.querySelector(".portfolio-swiper-two");
-if(portfolio02 ){
-const portfolioSwiper02 = new Swiper('.portfolio-swiper-two', {
-  spaceBetween: 25,
-  wrapperClass: "portfolio-swiper-wrapper-two",
-  slideClass: "portfolio-swiper-slide-two",
-  grabCursor: true,
-  loop: true,
-  observer: true,
-  observeParents: true,
-  slidesPerView:1.2,
-  navigation: {
-    nextEl: ".portfolio-button__next",
-    prevEl: ".portfolio-button__prev",
-  },
-  breakpoints: {
-  500: {
-    slidesPerView: 1.5,
-  },
-  1024: {
-    slidesPerView: 2,
-    spaceBetween: 30,
-  },
-  1440: {
-    slidesPerView: 2.2,
-  },
-  1920: {
-    slidesPerView: 2.7,
-  },
-}
-});
+if (portfolio02) {
+	const portfolioSwiper02 = new Swiper('.portfolio-swiper-two', {
+		spaceBetween: 25,
+		wrapperClass: "portfolio-swiper-wrapper-two",
+		slideClass: "portfolio-swiper-slide-two",
+		grabCursor: true,
+		loop: true,
+		observer: true,
+		observeParents: true,
+		slidesPerView: 1.2,
+		navigation: {
+			nextEl: ".portfolio-button__next",
+			prevEl: ".portfolio-button__prev",
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 1.5,
+			},
+			1024: {
+				slidesPerView: 2,
+				spaceBetween: 30,
+			},
+			1440: {
+				slidesPerView: 2.2,
+			},
+			1920: {
+				slidesPerView: 2.7,
+			},
+		}
+	});
 }
 const workSwiper1 = document.querySelector(".work__swiper-one");
-if(workSwiper1 ){
-  const workSwiper1 = new Swiper('.work__swiper-one', {
-  spaceBetween: 30,
-  wrapperClass: "work-swiper-wrapper-one",
-  slideClass: "work__swiper-slide-one",
-  slidesPerView: 4,
-  loop: true,
-  speed: 7000,
-  autoplay: {
-    delay: 0,
-  },
-  breakpoints: {
-  768: {
-    slidesPerView: 5,
-  },
-  1200: {
-    slidesPerView: 6,
-  },
-}
-})
+if (workSwiper1) {
+	const workSwiper1 = new Swiper('.work__swiper-one', {
+		spaceBetween: 30,
+		wrapperClass: "work-swiper-wrapper-one",
+		slideClass: "work__swiper-slide-one",
+		slidesPerView: 4,
+		loop: true,
+		speed: 7000,
+		autoplay: {
+			delay: 0,
+		},
+		breakpoints: {
+			768: {
+				slidesPerView: 5,
+			},
+			1200: {
+				slidesPerView: 6,
+			},
+		}
+	})
 }
 const workSwiper2 = document.querySelector(".work__swiper-two");
-if(workSwiper2 ){
-  const workSwiper2 = new Swiper('.work__swiper-two', {
-  spaceBetween: 30,
-  wrapperClass: "work-swiper-wrapper-two",
-  slideClass: "work__swiper-slide-two",
-  loop: true,
-  slidesPerView: 4,
-  speed: 7000,
-  autoplay: {
-    delay: 0,
-    reverseDirection:true,
-  },
-  breakpoints: {
-  768: {
-    slidesPerView: 5,
-  },
-  1200: {
-    slidesPerView: 6,
-  },
-}
-})
+if (workSwiper2) {
+	const workSwiper2 = new Swiper('.work__swiper-two', {
+		spaceBetween: 30,
+		wrapperClass: "work-swiper-wrapper-two",
+		slideClass: "work__swiper-slide-two",
+		loop: true,
+		slidesPerView: 4,
+		speed: 7000,
+		autoplay: {
+			delay: 0,
+			reverseDirection: true,
+		},
+		breakpoints: {
+			768: {
+				slidesPerView: 5,
+			},
+			1200: {
+				slidesPerView: 6,
+			},
+		}
+	})
 }
 
 const aboutSwiper1 = document.querySelector(".about__swiper-one");
-if(aboutSwiper1 ){
-  const aboutSwiper1  = new Swiper('.about__swiper-one', {
-  spaceBetween: 10,
-  wrapperClass: "about__swiper-wrapper-one",
-  slideClass: "about__swiper-slide-one",
-  slidesPerView: 3,
-  loop: true,
-  speed: 10000,
-  autoplay: {
-    delay: 0,
-    reverseDirection: true
-  },
-    followFinger: false,
-  breakpoints: {
-  768: {
-    slidesPerView: 5,
-    spaceBetween: 30
-  },
-  1200: {
-    slidesPerView: 6,
-    spaceBetween: 30
-  },
-}
-})
+if (aboutSwiper1) {
+	const aboutSwiper1 = new Swiper('.about__swiper-one', {
+		spaceBetween: 10,
+		wrapperClass: "about__swiper-wrapper-one",
+		slideClass: "about__swiper-slide-one",
+		slidesPerView: 3,
+		loop: true,
+		speed: 7000,
+		autoplay: {
+			delay: 0
+		},
+		followFinger: false,
+		breakpoints: {
+			768: {
+				slidesPerView: 5,
+				spaceBetween: 30
+			},
+			1200: {
+				slidesPerView: 6,
+				spaceBetween: 30
+			},
+		}
+	})
 }
 const aboutSwiper2 = document.querySelector(".about__swiper-two");
-if(aboutSwiper2 ){
-  const aboutSwiper2 = new Swiper('.about__swiper-two', {
-  spaceBetween: 10,
-  wrapperClass: "about-swiper-wrapper-two",
-  slideClass: "about__swiper-slide-two",
-  loop: true,
-  slidesPerView: 3,
-  speed: 10000,
-  autoplay: {
-    delay: 0,
-  },
-    followFinger: false,
-  breakpoints: {
-  768: {
-    slidesPerView: 5,
-    spaceBetween: 30
-  },
-  1200: {
-    slidesPerView: 6,
-    spaceBetween: 30
-  },
-}
-})
+if (aboutSwiper2) {
+	const aboutSwiper2 = new Swiper('.about__swiper-two', {
+		spaceBetween: 10,
+		wrapperClass: "about-swiper-wrapper-two",
+		slideClass: "about__swiper-slide-two",
+		loop: true,
+		slidesPerView: 3,
+		speed: 7000,
+		autoplay: {
+			delay: 0,
+			reverseDirection: true
+		},
+		followFinger: false,
+		breakpoints: {
+			768: {
+				slidesPerView: 5,
+				spaceBetween: 30
+			},
+			1200: {
+				slidesPerView: 6,
+				spaceBetween: 30
+			},
+		}
+	})
 }
